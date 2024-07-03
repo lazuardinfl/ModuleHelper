d1a8c6d30f1c71a95f6af302d2c5af0b6746b1c2
Lazuardi Naufal <lazuardi.n@samsung.com>
2024-07-03 11:16:30 +0700

    add initialize private gallery

---
+++ ModuleHelper.psm1
@@ -0,0 +1,25 @@
+using namespace System
+
+function Initialize-PrivateGallery {
+    param (
+        [Alias("GalleryName")] [string]$name,
+        [Alias("GalleryUrl")] [string]$url,
+        [Alias("OnErrorContinue")] [switch]$silent
+    )
+    try {
+        $name = $name ? $name : ($env:PrivatePSGallery ? "PrivatePSGallery" : $null)
+        $url = $url ? $url : [Environment]::GetEnvironmentVariable($name)
+        if (!$name) { throw "Environment variable PrivatePSGallery not found" }
+        if (!$url) { throw "Invalid url" }
+        $repo = Get-PSResourceRepository -Name $name -ErrorAction SilentlyContinue
+        if ($repo) {
+            if ($repo.Uri.ToString().TrimEnd('/') -ne $url.TrimEnd('/')) {
+                Set-PSResourceRepository -Name $name -Uri $url -ErrorAction Stop
+            }
+        }
+        else {
+            Register-PSResourceRepository -Name $name -Uri $url -Priority 100 -Trusted -ErrorAction Stop
+        }
+    }
+    catch { if ($silent) { return $null } else { throw } }
+}
